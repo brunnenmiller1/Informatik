c1bc4623298bb5a5545e1097614400686024ead2
brunnenmiller1 <phil.brunnenmiller@gmail.com>
2025-12-16 13:58:40 +0000

    aufgaben

---
+++ 0.3aufbaben.py
@@ -0,0 +1,20 @@
+#Aufgabe2
+
+print(bool(10 < 3));
+print(bool(5 == 5));
+print(bool(7 != 8));
+print(bool(12.5 >= 12.5));
+print(bool("S235" == "S355"));
+
+#Aufgabe3
+
+print(bool(0));
+print(bool(1));
+print(bool(-1));
+print(bool(0.0));
+print(bool(0.01));
+print(bool(""));
+print(bool("ok"));
+
+#Aufgabe4
+
